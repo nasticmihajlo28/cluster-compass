251883ec3b7ecc87042d3d958753a81cdc4ccaac
nasticmihajlo28 <nasticmihajlo28@gmail.com>
2026-09-09 20:28:30 +0200

    Update compass-stage.js

--- compass-stage.js
+++ compass-stage.js
@@ -71,7 +71,7 @@ const CONTENT = {
   welcomeSiteLabel: "davidallisoninc.com",
   emailTitle: "Take your cluster with you",
   emailBody: "Give us your email address. We'll send you more about your cluster and how to put it to work in your life.",
-  emailPlaceholder: "you@example.com",
+  emailPlaceholder: "Email",
   emailButton: "Send me my full report",
   emailInvalid: "That email doesn't look quite right.",
   emailError: "Something went wrong. Give it another try.",
